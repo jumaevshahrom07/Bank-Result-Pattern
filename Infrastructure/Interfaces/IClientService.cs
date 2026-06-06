78926029c1ca215d182c9a7d08ecc1379b8c7fde
Shahrom Jumaev <shahromdzumaev61@gmail.com>
2026-06-06 23:39:29 +0500

    Change some places

--- Infrastructure/Interfaces/IClientService.cs
+++ Infrastructure/Interfaces/IClientService.cs
@@ -5,7 +5,8 @@ namespace Infrastructure.Interfaces;
 
 public interface IClientService
 {
-Task<PagedResult<ClinetGetDto>> GetAllAsync(ClientFilterDto dto);    Task<Result<ClinetGetDto>> CreateAsync(ClientCreateDto dto);
+    Task<PagedResult<ClinetGetDto>> GetAllAsync(ClientFilterDto dto); 
+    Task<Result<ClinetGetDto>> CreateAsync(ClientCreateDto dto);
     Task<Result<string>> UpdateAsync(Guid id, ClientUpdateDto dto);
     Task<Result<string>> DeleteAsync(Guid id);
     Task<Result<ClinetGetDto>> GetByIdAsync(Guid id);
